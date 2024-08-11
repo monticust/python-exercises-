d9694c54f9a001764d14ae9e62b164426ff39c83
Monti <monticust@yahoo.com>
2024-08-11 13:32:01 +0100

    first commit

---
+++ word-count.py
@@ -0,0 +1,11 @@
+#my_string="one two three"
+my_string="one two " 
+words=my_string.split()
+print(f"my string is {my_string}")
+print(f"the list of words is {words}")
+#print(len(my_string))
+word_count=len(words)
+print(f"the number of words is {word_count}")
+
+
+
